6a1800a889ac273666ec549174bf9591ea2438be
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-24 23:36:24 +0330

    solving run time error via removing service from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
 import { CategoryModule } from './category/category.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, UsersService, CategoryModule, ProductsModule, AuthModule],
+  imports: [PrismaModule, UsersModule, CategoryModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
