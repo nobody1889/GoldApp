fe0ff4ccd8d1207c469c40a3ed0eb45542509b94
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-25 00:46:24 +0330

    add logger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    {
+      logger: [
+        'log',
+        'error',
+        'warn',
+      ]
+    }
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
