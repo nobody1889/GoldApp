96caec1b5e24ea5cae0769c6f0336287a16bc354
nobody1889 <nobodyisherefornow1977@gmail.com>
2025-12-25 23:17:21 +0330

    solve local guard problem

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,21 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'phoneNumber', // or 'email'
+    });
+  }
+
+  async validate(phoneNumber: string, password: string) {
+    const user = await this.authService.validateUser(phoneNumber, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
